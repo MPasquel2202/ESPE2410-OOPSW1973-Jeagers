b9884732f6a7ffba08ec1e099aafdae19bf30c07
Brandon <pazminob558@gmail.com>
2024-12-07 20:12:42 -0500

    CodeMoleMenuV1.0.0

--- 06-Code/MoleManagerSystem/src/ec/edu/espe/projects/view/ProjectsApp.java
+++ 06-Code/MoleManagerSystem/src/ec/edu/espe/projects/view/ProjectsApp.java
@@ -5,8 +5,44 @@ package ec.edu.espe.projects.view;
  * @author Dennis Paucar
  */
 public class ProjectsApp {
+
     public static void main(String[] args) {
         System.out.println("MOLE Project System Manager");
+        int menuOptions;
+        do {
+            System.out.println("""
+                      Hello, select any function.
+                      1.Create a new project folder
+                      2.View projects
+                      3.Profile
+                      4.Generate reports
+                      5.Settings
+                      6.Help
+                      """);
+
+            switch (menuOptions) {
+                case 1:
+            
+            ;
+                case 2:
+            
+            ;
+                case 3:
+            
+            ;
+                case 4:
+            
+            ;
+                case 5:
+            
+            ;
+                case 6:
+            
+            ;
+
+            }
+        } while (numberOptions = > 1 && numberOptions = < 6)
+ 
     }
-    
+
 }

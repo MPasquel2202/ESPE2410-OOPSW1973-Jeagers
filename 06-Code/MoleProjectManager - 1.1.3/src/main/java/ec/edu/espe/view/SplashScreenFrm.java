0f4c0926576f086bd06d3ba173f8817e7c7d9d15
DennisPaucar <dennispaucar49@gmail.com>
2025-03-10 10:29:19 -0500

    sd

--- 06-Code/MoleProjectManager - 1.1.3/src/main/java/ec/edu/espe/view/SplashScreenFrm.java
+++ 06-Code/MoleProjectManager - 1.1.3/src/main/java/ec/edu/espe/view/SplashScreenFrm.java
@@ -121,6 +121,205 @@ public class SplashScreenFrm extends javax.swing.JFrame {
         });
         mTimer.start(); 
     }
+    
+//    Project project = new Project.Builder("P001", "Nuevo Proyecto")
+//    .setProjectDescription("Descripción del proyecto")
+//    .setCustomer(customer)
+//    .setStartDate(new Date())
+//    .setClosingDate(new Date())
+//    .setStartquote(5000)
+//    .setOperationalStatus(ProjectStatus.ACTIVE)
+//    .setQuoteStatus(ProjectStatus.PENDING)
+//    .setPaid(true)
+//    .setInvoiced(false)
+//    .setPublic(true)
+//    .setResponsiblePerson("Juan Pérez")
+//    .build();
+    
+
+//public class Project implements ProjectData {
+//
+//    private String projectId;
+//    private String projectTitle;
+//    private String projectDescription;
+//    private Customer customer;
+//    private Date startDate;
+//    private Date closingDate;
+//    private double startquote;
+//    private ProjectStatus operationalStatus;
+//    private ProjectStatus quoteStatus;
+//    private boolean paid;
+//    private boolean invoiced;
+//    private boolean isPublic;
+//    private String responsiblePerson;  // Nuevo campo
+//
+//    private Project(Builder builder) {
+//        this.projectId = builder.projectId;
+//        this.projectTitle = builder.projectTitle;
+//        this.projectDescription = builder.projectDescription;
+//        this.customer = builder.customer;
+//        this.startDate = builder.startDate;
+//        this.closingDate = builder.closingDate;
+//        this.startquote = builder.startquote;
+//        this.operationalStatus = builder.operationalStatus;
+//        this.quoteStatus = builder.quoteStatus;
+//        this.paid = builder.paid;
+//        this.invoiced = builder.invoiced;
+//        this.isPublic = builder.isPublic;
+//        this.responsiblePerson = builder.responsiblePerson; // Asignar el nuevo campo
+//    }
+//
+//    @Override
+//    public String getProjectId() {
+//        return projectId;
+//    }
+//
+//    @Override
+//    public String getProjectTitle() {
+//        return projectTitle;
+//    }
+//
+//    @Override
+//    public String getProjectDescription() {
+//        return projectDescription;
+//    }
+//
+//    @Override
+//    public Customer getCustomer() {
+//        return customer;
+//    }
+//
+//    @Override
+//    public Date getStartDate() {
+//        return startDate;
+//    }
+//
+//    @Override
+//    public Date getClosingDate() {
+//        return closingDate;
+//    }
+//
+//    @Override
+//    public double getStartquote() {
+//        return startquote;
+//    }
+//
+//    @Override
+//    public ProjectStatus getOperationalStatus() {
+//        return operationalStatus;
+//    }
+//
+//    @Override
+//    public ProjectStatus getQuoteStatus() {
+//        return quoteStatus;
+//    }
+//
+//    @Override
+//    public boolean isPaid() {
+//        return paid;
+//    }
+//
+//    @Override
+//    public boolean isInvoiced() {
+//        return invoiced;
+//    }
+//
+//    @Override
+//    public boolean isPublic() {
+//        return isPublic;
+//    }
+//
+//    public String getResponsiblePerson() {  // Getter para el nuevo campo
+//        return responsiblePerson;
+//    }
+//
+//    public void setStartquote(double startquote) {
+//        this.startquote = startquote;
+//    }
+//
+//    public static class Builder {
+//
+//        private String projectId;
+//        private String projectTitle;
+//        private String projectDescription;
+//        private Customer customer;
+//        private Date startDate;
+//        private Date closingDate;
+//        private double startquote;
+//        private ProjectStatus operationalStatus;
+//        private ProjectStatus quoteStatus;
+//        private boolean paid;
+//        private boolean invoiced;
+//        private boolean isPublic;
+//        private String responsiblePerson; // Campo para la persona a cargo
+//
+//        public Builder(String projectId, String projectTitle) {
+//            this.projectId = projectId;
+//            this.projectTitle = projectTitle;
+//        }
+//
+//        public Builder setProjectDescription(String projectDescription) {
+//            this.projectDescription = projectDescription;
+//            return this;
+//        }
+//
+//        public Builder setCustomer(Customer customer) {
+//            this.customer = customer;
+//            return this;
+//        }
+//
+//        public Builder setStartDate(Date startDate) {
+//            this.startDate = startDate;
+//            return this;
+//        }
+//
+//        public Builder setClosingDate(Date closingDate) {
+//            this.closingDate = closingDate;
+//            return this;
+//        }
+//
+//        public Builder setStartquote(double startquote) {
+//            this.startquote = startquote;
+//            return this;
+//        }
+//
+//        public Builder setOperationalStatus(ProjectStatus operationalStatus) {
+//            this.operationalStatus = operationalStatus;
+//            return this;
+//        }
+//
+//        public Builder setQuoteStatus(ProjectStatus quoteStatus) {
+//            this.quoteStatus = quoteStatus;
+//            return this;
+//        }
+//
+//        public Builder setPaid(boolean paid) {
+//            this.paid = paid;
+//            return this;
+//        }
+//
+//        public Builder setInvoiced(boolean invoiced) {
+//            this.invoiced = invoiced;
+//            return this;
+//        }
+//
+//        public Builder setPublic(boolean isPublic) {
+//            this.isPublic = isPublic;
+//            return this;
+//        }
+//
+//        public Builder setResponsiblePerson(String responsiblePerson) {  // Método para establecer la persona a cargo
+//            this.responsiblePerson = responsiblePerson;
+//            return this;
+//        }
+//
+//        public Project build() {
+//            return new Project(this);
+//        }
+//    }
+//}
+
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
